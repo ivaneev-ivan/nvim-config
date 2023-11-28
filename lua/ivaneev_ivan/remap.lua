9e120437a4595786b6852794b1adaa81ce078ca8
ivan <ivaneev.ivane@gmail.com>
2023-11-28 23:40:38 +0300

    Disable autocomplete if typing and change some keymaps

--- lua/ivaneev_ivan/remap.lua
+++ lua/ivaneev_ivan/remap.lua
@@ -1,5 +1,6 @@
 vim.g.mapleader = " "
-vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+vim.keymap.set("n", "<leader>e", vim.cmd.Ex)
 vim.keymap.set("i", "jk", "<esc>")
 vim.keymap.set("n", "H", "gT")
 vim.keymap.set("n", "L", "gt")
+vim.keymap.set("n", ",<leader>", ":nohlsearch<CR>")
